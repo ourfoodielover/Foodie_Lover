0e934b8624471956289351c6d918535c44a12b6c
Manish Racha <luckymanish02@gmail.com>
2026-03-09 19:36:57 -0500

    chore: add version comment to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* config options here */ // Foodie Lover v2.0
 };
 
 export default nextConfig;
